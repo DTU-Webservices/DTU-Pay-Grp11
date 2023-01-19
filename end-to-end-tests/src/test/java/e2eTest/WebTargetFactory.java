0dc81af8c75f2dc9bfa8abf7b7fa2fb03259b78b
Kristoffer <kristoffer@kritix.dk>
2023-01-19 13:55:46 +0100

    Updated build enviroment

--- end-to-end-tests/src/test/java/e2eTest/WebTargetFactory.java
+++ end-to-end-tests/src/test/java/e2eTest/WebTargetFactory.java
@@ -4,11 +4,12 @@ import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-public class WebTargetFactory {
 
+public class WebTargetFactory {
     static Client client;
 
     public static WebTarget getWebTarget() {
+
         client = ClientBuilder.newClient();
         return client.target("http://localhost:8080/");
     }
