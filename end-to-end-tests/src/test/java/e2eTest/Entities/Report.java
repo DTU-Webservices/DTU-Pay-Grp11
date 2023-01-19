ed1d60e6de84b5749720c707da26a095b958733e
Kristoffer <kristoffer@kritix.dk>
2023-01-19 18:46:37 +0100

    Added many tests

---
+++ end-to-end-tests/src/test/java/e2eTest/Entities/Report.java
@@ -0,0 +1,22 @@
+package e2eTest.Entities;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.UUID;
+
+/**
+ *
+ * @author Oliver Brink Klenum s193625
+ *
+ */
+@Data
+public class Report implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private UUID reportId;
+    private Set<MoneyTransfer> moneyTransfers;
+    private BigDecimal totalAmount = null;
+}
--- end-to-end-tests/src/test/java/e2eTest/TestService.java
+++ end-to-end-tests/src/test/java/e2eTest/TestService.java
@@ -8,48 +8,88 @@ import java.util.UUID;
 public class TestService {
 
     public Merchant registerMerchant(Merchant merchant) {
-        return WebTargetFactory.getWebTarget().path("merchants")
+        var response = WebTargetFactory.getWebTarget().path("merchants")
                 .request()
                 .post(Entity.json(merchant), Merchant.class);
+        WebTargetFactory.close();
+        return response;
     }
 
     public Merchant getMerchant(UUID merchantId) {
-        return WebTargetFactory.getWebTarget().path("merchants")
+        var response = WebTargetFactory.getWebTarget().path("merchants")
                 .path(merchantId.toString())
                 .request()
                 .get(Merchant.class);
+        WebTargetFactory.close();
+        return response;
     }
 
     public Customer registerCustomer(Customer customer) {
-        return WebTargetFactory.getWebTarget().path("customers")
+        var response = WebTargetFactory.getWebTarget().path("customers")
                 .request()
                 .post(Entity.json(customer), Customer.class);
+        WebTargetFactory.close();
+        return response;
     }
 
     public Customer getCustomer(UUID customerId) {
-        return WebTargetFactory.getWebTarget().path("customers")
+        var response = WebTargetFactory.getWebTarget().path("customers")
                 .path(customerId.toString())
                 .request()
                 .get(Customer.class);
+        WebTargetFactory.close();
+        return response;
     }
 
     public String generateTokens(Token token) {
-        return WebTargetFactory.getWebTarget().path("customers/tokens")
+        var response = WebTargetFactory.getWebTarget().path("customers/tokens")
                 .request()
                 .post(Entity.json(token), String.class);
+        WebTargetFactory.close();
+        return response;
     }
 
     public String getToken(UUID customerId) {
-        return WebTargetFactory.getWebTarget().path("customers/tokens")
+        var response = WebTargetFactory.getWebTarget().path("customers/tokens")
                 .path(customerId.toString())
                 .request()
                 .get(String.class);
+        WebTargetFactory.close();
+        return response;
     }
 
     public MoneyTransfer makePaymentAndTransferMoney(Payment payment) {
-        return WebTargetFactory.getWebTarget().path("merchants/payment")
+        var response = WebTargetFactory.getWebTarget().path("merchants/payment")
                 .request()
                 .post(Entity.json(payment), MoneyTransfer.class);
+        WebTargetFactory.close();
+        return response;
+    }
+
+    public Report getAllPaymentsForManager() {
+        var response = WebTargetFactory.getWebTarget().path("reports")
+                .request()
+                .get(Report.class);
+        WebTargetFactory.close();
+        return response;
+    }
+
+    public Report getAllPaymentsMadeByCustomer(UUID customerId) {
+        var response = WebTargetFactory.getWebTarget().path("customers/reports")
+                .path(customerId.toString())
+                .request()
+                .get(Report.class);
+        WebTargetFactory.close();
+        return response;
+    }
+
+    public  Report getAllPaymentsMadeByMerchant(UUID merchantId) {
+        var response = WebTargetFactory.getWebTarget().path("merchants/reports")
+                .path(merchantId.toString())
+                .request()
+                .get(Report.class);
+        WebTargetFactory.close();
+        return response;
     }
 }
 
